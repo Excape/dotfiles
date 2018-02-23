fa9ac864caf2a6020281e5e1fdcdde90189fa8d3
Robin Suter <suter.robin@gmail.com>
2018-02-23 08:11:01 +0100

    new nix packages

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -85,6 +85,17 @@
       ];
     };
 
+    eclipse-ee-47 = pkgs.eclipses.buildEclipse {
+      name = "eclipse-ee-4.7";
+      description = "Eclipse EE IDE";
+      sources = {
+        "x86_64-linux" = pkgs.fetchurl {
+          url = https://www.eclipse.org/downloads/download.php?r=1&nf=1&file=/technology/epp/downloads/release/oxygen/R/eclipse-jee-oxygen-R-linux-gtk-x86_64.tar.gz;
+          sha256 = "1w5l1sxqzb6w2akd8bl74qsm5a3bkrhb55fvfa9mlrdbdr5cj7vj";
+          name = "eclipse-jee-oxygen-R-linux-gtk-x86_64.tar.gz";
+        };
+      };
+    };
 
     ### --- custom packages ---
 
@@ -138,8 +149,9 @@
       paths = [ 
         firefox
         keepassx-community
+        keepass
         thunderbird
-        libreoffice
+        libreoffice-fresh
         xfce.thunar
         nextcloud-client
         sxiv # image viewer
@@ -159,11 +171,21 @@
       ];
     };
 
+    cloudSolApps = with pkgs; buildEnv {
+      name = "devApps";
+      paths = [ 
+        eclipse-ee-47
+        python # for gcloud
+        #google-cloud-sdk
+      ];
+    };
+
     devApps = with pkgs; buildEnv {
       name = "devApps";
       paths = [ 
         jetbrains.pycharm-professional
         myVscode
+        git-crypt
       ];
     };
 
